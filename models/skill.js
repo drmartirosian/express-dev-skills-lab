a1c8b5b1e72fabc5254e05829ed87582d52afb75
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-11-20 03:59:00 -0600

    stubbed up models

---
+++ models/skill.js
@@ -0,0 +1,38 @@
+//skill array
+const skills = [
+    {skill:HTML, done: true},
+    {skill:CSS, done: true},
+    {skill:JavaScript, done: true},
+    {skill:NodeJS, done: true},
+    {skill:MongoDB, done: true},
+    {skill:Python, done: false},
+    {skill:React, done: false},
+    {skill:Ruby, done: false},
+    {skill:RubyonRails, done: false},
+    {skill:Java, done: false},
+];
+
+//export 
+module.export = {
+    getAll,
+    getOne,
+    create,
+    update,
+    deleteOne,
+};
+
+function getAll(){
+    return skills;
+};
+function getOne(id){
+    return skills[id];
+};
+function create(skill){
+    skills.push(skill);
+};
+function update(id, skill){
+    skills.splice(id, 1, skill);
+};
+function deleteOne(id){
+    skills.splice(id, 1);
+};
